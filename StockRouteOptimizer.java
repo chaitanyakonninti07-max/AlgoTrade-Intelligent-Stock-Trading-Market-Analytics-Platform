a2f7b2bc0f975a5e4ba63bd09971d84e47f820bf
chaitanyakonninti07-max <chaitanyakonninti07@gmail.com>
2026-06-10 08:56:51 +0530

    Add files via upload

---
+++ StockRouteOptimizer.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class StockRouteOptimizer {
+
+    static final int V = 6;
+
+    int minDistance(int[] dist, boolean[] visited) {
+        int min = Integer.MAX_VALUE;
+        int minIndex = -1;
+
+        for (int v = 0; v < V; v++) {
+            if (!visited[v] && dist[v] < min) {
+                min = dist[v];
+                minIndex = v;
+            }
+        }
+        return minIndex;
+    }
+
+    void dijkstra(int[][] graph, int src) {
+
+        int[] dist = new int[V];
+        boolean[] visited = new boolean[V];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        for (int count = 0; count < V - 1; count++) {
+
+            int u = minDistance(dist, visited);
+
+            if (u == -1)
+                break;
+
+            visited[u] = true;
+
+            for (int v = 0; v < V; v++) {
+
+                if (!visited[v]
+                        && graph[u][v] != 0
+                        && dist[u] != Integer.MAX_VALUE
+                        && dist[u] + graph[u][v] < dist[v]) {
+
+                    dist[v] = dist[u] + graph[u][v];
+                }
+            }
+        }
+
+        System.out.println("Shortest Distances from Source Node 0:");
+
+        for (int i = 0; i < V; i++) {
+            System.out.println("Node " + i + " = " + dist[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] graph = {
+                {0, 4, 2, 0, 0, 0},
+                {4, 0, 1, 5, 0, 0},
+                {2, 1, 0, 8, 10, 0},
+                {0, 5, 8, 0, 2, 6},
+                {0, 0, 10, 2, 0, 3},
+                {0, 0, 0, 6, 3, 0}
+        };
+
+        StockRouteOptimizer obj = new StockRouteOptimizer();
+
+        obj.dijkstra(graph, 0);
+    }
+}
