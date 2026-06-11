90f87e0199094ab2ab7601693643cd49c30fca74
chaitanyakonninti07-max <chaitanyakonninti07@gmail.com>
2026-06-11 09:01:17 +0530

    Add files via upload

---
+++ StockSortingAnalysis.java
@@ -0,0 +1,81 @@
+public class StockSortingAnalysis {
+
+    void merge(int arr[], int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(int arr[], int left, int right) {
+
+        if (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    void printArray(int arr[]) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int stockPrices[] = {220, 150, 300, 180, 250};
+
+        StockSortingAnalysis obj =
+                new StockSortingAnalysis();
+
+        System.out.println("Stock Prices Before Sorting:");
+        obj.printArray(stockPrices);
+
+        obj.mergeSort(stockPrices, 0,
+                stockPrices.length - 1);
+
+        System.out.println("\nStock Prices After Sorting:");
+        obj.printArray(stockPrices);
+    }
+}
